6dc307c938df8d01eb10c883f0af7e4703d5caab
Joshua Diaz <dijoshua0323@gmail.com>
2025-06-07 19:36:20 -0500

    final JS

--- README.md
+++
@@ -1,2 +0,0 @@
-# WEBSITE RECREATION PROJECT
-In this project, I recreated the IN-N-OUT Burger website. 
--- script.js
+++ script.js
@@ -1,41 +1,55 @@
 //email validation
-function validateEmail(email){
-    if(email.includes ("@") && email.includes (".")){
-        return true;
+function validateEmail(email) {
+    if (email.includes("@") && email.includes(".")) {
+         return true;
     } else {
         return false;
     }
-}        
+}
 console.log(validateEmail("joshdiaz@email.com"))
+console.log(validateEmail("joshdiazemailcom"))
+
+//onclick event
+function greet(){
+    alert("Form submitted succesful")
+}
 
 //search for locations
-function searchLocations(){
+function searchLocations() {
+    //check if your state has a store
+    //list of valid locations
     let location = ["California", "Nevada", "Arizona", "Texas"]
-    const userInput = prompt('Type in your state name in full:')
-    
-} 
-console.log(location.includes(userInput))
-
+    //typing your location
+    //const userInput = prompt('Type in your state name in full:')
+    const userInput = "Nevada"
+    //checks your location has a store
+    return location.includes(userInput)
+}
+console.log(searchLocations)
 
 
 
 
 //Shopping cart
-const cart= []
-function addToCart(){
-cart.push({item:1, name: "cellphone", price: 1000.00});
-cart.push({item: 2, name: "laptop", price: 1500.00})
+const cart = []
+function addToCart() {
+    cart.push({ item: 1, name: "cellphone", price: 1000.00 });
+    cart.push({ item: 2, name: "laptop", price: 1500.00 })
+    console.log(cart)
 }
-console.log(cart)
+
 
 //card validation
 
-cardId = [1234,4567,7890]
-function validateCard(inputId){
-  
-  if(cardId.includes(inputId))
-    console.log("Card Accepted")
-} else{
-  console.log("Card not valid")
+cardIds = [1234, 4567, 7890, 1123, 3456]
+function validateCard(inputId) {
+    if (cardIds.includes(inputId)) {
+        console.log("Card Accepted")
+    }
+
+    else {
+        console.log("Card not valid")
+    }
 }
+
 
